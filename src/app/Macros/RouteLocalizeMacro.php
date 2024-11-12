53bfc3c3dacef40cc37cf15b3c2cb5409787a0e6
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2024-11-12 18:10:10 +0100

    Fixed bug when using Laravel Octane

--- src/app/Macros/RouteLocalizeMacro.php
+++ src/app/Macros/RouteLocalizeMacro.php
@@ -30,7 +30,7 @@ class RouteLocalizeMacro implements MacroContract
                 Route::group(['prefix' => app()->getLocale(), 'locale' => app()->getLocale()], $callback);
 
                 $isCurrentPrefixedByALocale = array_reduce(available_locales(), function ($carry, $locale) {
-                    return $carry || str_starts_with(request()->path(), $locale . '/');
+                    return $carry || str_starts_with(request()->path(), $locale . '/') || request()->path() == $locale;
                 }, false);
                 
                 if (!$isCurrentPrefixedByALocale) {
@@ -68,34 +68,24 @@ class RouteLocalizeMacro implements MacroContract
                 'callback' => $callback,
             ];
 
-            // Register localized routes for the other available locales
-            collect(app(RouteLocalizationService::class)->getAvailableLocales(true))
-                ->each(function ($locale) use ($callback) {
-                    if (config('localized-routes.prefix_default_locale')) {
-                        Route::group(['prefix' => $locale, 'as' => "{$locale}.", 'locale' => $locale], $callback);
-                    } else {
-                        // Add compatibility with route helper
-                        Route::when(!is_default_locale($locale), fn () => Route::group(['prefix' => $locale, 'as' => "{$locale}.", 'locale' => $locale], $callback));
-                        Route::when(is_default_locale($locale), fn () => Route::group(['as' => "{$locale}.", 'locale' => $locale], $callback)); // Do not prefix the default locale
-                    }
-                });
-
             return $this;
         });
     }
 
     protected static function registerRouteRegisterLocalizedRoutesForLocaleForOctane(): void
     {
-        Route::macro('registerLocalizedRoutesForLocale', function ($locale) {
+        Route::macro('registerLocalizedRoutesForLocale', function ($currentLocale) {
             collect($this->callbackToLocalize ?? [])
-                ->each(function ($callback) use ($locale) {
+                ->each(function ($callback) use ($currentLocale) {
+                    // Restore the group stack
                     $this->groupStack = $callback['groupStack'];
+
                     // Prefix the current route with the right locale
                     if (config('localized-routes.prefix_default_locale')) {
-                        Route::group(['prefix' => $locale, 'locale' => $locale], $callback['callback']);
-            
-                        $isCurrentPrefixedByALocale = array_reduce(available_locales(), function ($carry, $locale) {
-                            return $carry || str_starts_with(request()->path(), $locale . '/');
+                        Route::group(['prefix' => $currentLocale, 'locale' => $currentLocale], $callback['callback']);
+
+                        $isCurrentPrefixedByALocale = array_reduce(app(RouteLocalizationService::class)->getAvailableLocales(), function ($carry, $locale) {
+                            return $carry || str_starts_with(request()->path(), $locale . '/') || request()->path() == $locale;
                         }, false);
                         
                         if (!$isCurrentPrefixedByALocale) {
@@ -103,13 +93,28 @@ class RouteLocalizeMacro implements MacroContract
                         }
                     } else {
                         // Add compatibility with route helper
-                        Route::when(!is_default_locale($locale), fn () => Route::group(['prefix' => $locale, 'locale' => $locale], $callback));
-                        Route::when(is_default_locale($locale), fn () => Route::group(['locale' => $locale], $callback)); // Do not prefix the default locale
+                        Route::when(!is_default_locale($currentLocale), fn () => Route::group(['prefix' => $currentLocale, 'locale' => $currentLocale], $callback['callback']));
+                        Route::when(is_default_locale($currentLocale), fn () => Route::group(['locale' => $currentLocale], $callback['callback'])); // Do not prefix the default locale
                     }
+
+                    // Register localized routes for the other available locales
+                    collect(app(RouteLocalizationService::class)->getAvailableLocales())
+                        ->filter(fn ($locale) => $locale != $currentLocale)
+                        ->each(function ($locale) use ($callback) {
+                            if (config('localized-routes.prefix_default_locale')) {
+                                Route::group(['prefix' => $locale, 'as' => "{$locale}.", 'locale' => $locale], $callback['callback']);
+                            } else {
+                                // Add compatibility with route helper
+                                Route::when(!is_default_locale($locale), fn () => Route::group(['prefix' => $locale, 'as' => "{$locale}.", 'locale' => $locale], $callback['callback']));
+                                Route::when(is_default_locale($locale), fn () => Route::group(['as' => "{$locale}.", 'locale' => $locale], $callback['callback'])); // Do not prefix the default locale
+                            }
+                        });
                 });
 
             Route::getRoutes()->refreshNameLookups();
             Route::getRoutes()->refreshActionLookups();
+
+            return $this;
         });
     }
 
